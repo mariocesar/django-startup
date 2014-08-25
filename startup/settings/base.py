f2aca233782a971d3d836f66007ea59358fac1ae
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-08-25 01:54:36 -0400

    Common third party apps

--- startup/settings/base.py
+++ startup/settings/base.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
--- startup/settings/develop.py
+++ startup/settings/develop.py
@@ -4,6 +4,10 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
 # Host and Port to use with `maildump`
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
